36617f538051fd86052ba4486f20672ca7598277
Javier Ale <javierale25@live.com>
2021-05-28 21:37:27 -0300

    Environment.prod modified

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // Users API
+  URL: 'https://randomuser.me/api/?',
+
+  // Posts API
+  POSTS_URL: 'https://bikewise.org:443/api/v2/incidents',
+
+  // Tags API
+  TAGS_URL: 'https://dummyapi.io/data/api/tag?',
+  API_KEY: '60abbfa4d68ac91cdbc1bd04'
 };
