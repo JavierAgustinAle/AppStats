d43bb9157a844f9ba04948bed932282a8fa3aded
Javier Ale <javierale25@live.com>
2021-06-07 21:01:04 -0300

    Added method to sort by length or alphabetically tags

--- src/app/Components/tags/tags.component.ts
+++ src/app/Components/tags/tags.component.ts
@@ -9,7 +9,8 @@ import { TagService } from '../../Services/tag.service';
 })
 export class TagsComponent implements OnInit {
 
-  tags: ITag[];
+  tags: any[];
+  order: string = 'desc';
 
   constructor(private tagService: TagService) { }
 
@@ -19,4 +20,20 @@ export class TagsComponent implements OnInit {
     });
   }
 
+  sortAlphabet(): void {
+    this.tags.sort(function (a, b) {
+      var n = a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase());
+      return n === 0 && a !== b ? b.localeCompare(a) : n;
+    });
+  }
+
+  sortLength(): void {
+    if (this.order === 'desc') {
+      this.tags.sort((a, b) => a.length - b.length);
+      this.order = 'asc';
+    } else {
+      this.tags.sort((a, b) => b.length - a.length);
+      this.order = 'desc';
+    }
+  }
 }
